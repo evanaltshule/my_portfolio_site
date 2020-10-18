9a0cc32399bf40bbcff702f75a288ad48ba8b9ba
Evan Altshule <evanaltshule@Evans-MacBook-Pro.local>
2020-10-18 12:51:00 -0700

    resettig repo

---
+++ precache-manifest.0740196a2f78bda3610e5a44c147aa31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1a73ec3279ff40083c75cf56a71290",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e75a33cdf3a8e17f36d4",
+    "url": "/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "8788cbe57d0e74600e20",
+    "url": "/static/css/main.5fabbf80.chunk.css"
+  },
+  {
+    "revision": "e75a33cdf3a8e17f36d4",
+    "url": "/static/js/2.10da3ea4.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.10da3ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8788cbe57d0e74600e20",
+    "url": "/static/js/main.be526548.chunk.js"
+  },
+  {
+    "revision": "5eeb8d0fd802ead6c0b5",
+    "url": "/static/js/runtime-main.aa4df6ff.js"
+  },
+  {
+    "revision": "34724fb96508b6586b4508e3335730fc",
+    "url": "/static/media/hk.34724fb9.jpeg"
+  }
+]);
